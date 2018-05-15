b8718ea509877b2f9bff05f19c6f9013ae0d687e
palakm <39305866+palakm@users.noreply.github.com>
2018-05-15 20:12:45 +0530

    comit
    commit

---
+++ WebAPIExample/WebAPIExample/App_Start/DAWebAPI/DAEmployee.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPIExample.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebAPIExample.DataAccess
+{
+    public class DAEmployee
+    {
+
+
+        public List<Employee> GetAllEmployees()
+        {
+            List<Employee> lstemployees = new List<Employee>();
+            SqlDataReader reader = null;
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Server=localhost;Database=DBEmployee;User id=user1;Password=password1!;Integrated Security=False";
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "Select * from tblEmployee";
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            reader = sqlCmd.ExecuteReader();
+            Employee emp = null;
+            while (reader.Read())
+            {
+                emp = new Employee();
+                emp.EmployeeID = Convert.ToInt32(reader.GetValue(0));
+                emp.EmployeeFirstName = Convert.ToString(reader.GetValue(1));
+                emp.EmployeeLastName = Convert.ToString(reader.GetValue(2));
+                emp.ManagerID = Convert.ToInt32(reader.GetValue(3));
+                lstemployees.Add(emp);
+
+            }
+            return lstemployees;
+
+        }
+        public int AddEmployee(Employee objEmployee)
+        {
+
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Server=localhost;Database=DBEmployee;User id=user1;Password=password1!;Integrated Security=False";
+            //SqlCommand sqlCmd = new SqlCommand("INSERT INTO tblEmployee (EmployeeId,Name,ManagerId) Values (@EmployeeId,@Name,@ManagerId)", myConnection);
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "INSERT INTO tblEmployee (FirstName,LastName,ManagerId) Values (@EmployeeFirstName,@EmployeeLastName,@ManagerId)";
+            sqlCmd.Connection = myConnection;
+
+
+            sqlCmd.Parameters.AddWithValue("@EmployeeFirstName", objEmployee.EmployeeFirstName);
+            sqlCmd.Parameters.AddWithValue("@EmployeeLastName", objEmployee.EmployeeLastName);
+            sqlCmd.Parameters.AddWithValue("@ManagerId", objEmployee.ManagerID);
+            myConnection.Open();
+            int rowInserted = sqlCmd.ExecuteNonQuery();
+            myConnection.Close();
+            return rowInserted;
+
+        }
+
+        public int UpdateEmployee(Employee objEmployee)
+        {
+
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Server=localhost;Database=DBEmployee;User id=user1;Password=password1!;Integrated Security=False";
+            //SqlCommand sqlCmd = new SqlCommand("INSERT INTO tblEmployee (EmployeeId,Name,ManagerId) Values (@EmployeeId,@Name,@ManagerId)", myConnection);
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "update tblEmployee set FirstName=@EmployeeFirstName,LastName=@EmployeeLastName,ManagerId=@ManagerId where EmployeeID=@EmployeeID";
+            sqlCmd.Connection = myConnection;
+            sqlCmd.Parameters.AddWithValue("@EmployeeFirstName", objEmployee.EmployeeFirstName);
+            sqlCmd.Parameters.AddWithValue("@EmployeeLastName", objEmployee.EmployeeLastName);
+            sqlCmd.Parameters.AddWithValue("@ManagerId", objEmployee.ManagerID);
+            sqlCmd.Parameters.AddWithValue("@EmployeeID", objEmployee.EmployeeID);
+            myConnection.Open();
+            int rowupdated = sqlCmd.ExecuteNonQuery();
+            myConnection.Close();
+            return rowupdated;
+
+        }
+        public int DeleteEmployee(Employee objEmployee)
+        {
+
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Server=localhost;Database=DBEmployee;User id=user1;Password=password1!;Integrated Security=False";
+            //SqlCommand sqlCmd = new SqlCommand("INSERT INTO tblEmployee (EmployeeId,Name,ManagerId) Values (@EmployeeId,@Name,@ManagerId)", myConnection);
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "Delete from tblEmployee where EmployeeID=@EmployeeID";
+            sqlCmd.Connection = myConnection;
+            sqlCmd.Parameters.AddWithValue("@EmployeeID", objEmployee.EmployeeID);
+            myConnection.Open();
+            int rowupdated = sqlCmd.ExecuteNonQuery();
+            myConnection.Close();
+            return rowupdated;
+
+        }
+
+    }
+}
---
+++ WebAPIExample/WebAPIExample/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIExample.Models;
+using System.Data;
+using System.Data.SqlClient;
+using WebAPIExample.DataAccess;
+
+namespace WebAPIExample.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+
+        [HttpGet]
+        [ActionName("GetallEmployees")]
+        public List<Employee> GetEmployees()
+        {
+            DAEmployee objDAEmployee = new DAEmployee();
+            List<Employee> lstemployees = new List<Employee>();
+            lstemployees = objDAEmployee.GetAllEmployees();
+            return lstemployees;
+        }
+
+        [HttpPost]
+        public void AddEmployee([FromBody]Employee objemployee)
+        {
+            int Result = 0;
+            DAEmployee objDAEmployee = new DAEmployee();
+            Result = objDAEmployee.AddEmployee(objemployee);
+
+        }
+
+
+        [HttpPut]
+        [ActionName("UpdateEmployee")]
+        public void UpdateEmployee(Employee objemployee)
+        {
+            int Result = 0;
+            DAEmployee objDAEmployee = new DAEmployee();
+            Result = objDAEmployee.UpdateEmployee(objemployee);
+
+        }
+
+
+        [HttpDelete]
+        [ActionName("DeleteEmployee")]
+        public void DeleteEmployee(int ID)
+        {
+            int Result = 0;
+            DAEmployee objDAEmployee = new DAEmployee();
+            Employee objEmployee = new Employee();
+            objEmployee.EmployeeID = ID;
+            Result = objDAEmployee.DeleteEmployee(objEmployee);
+
+        }
+    }
+}
